fbcac1500e47038140b3eb8225515041f12ed7d4
BerensRWU <58558898+BerensRWU@users.noreply.github.com>
2023-01-10 16:26:27 +0100

    Update metrics.py

--- metrics.py
+++ metrics.py
@@ -1,12 +1,14 @@
 import numpy as np
-from scipy.spatial import distance_matrix
+from scipy.spatial.distance import cdist
+from scipy.spatial.distance import pdist
+
 """
 Shape of the point clouds:
 pc1.shape = (NumberOfPoints1, Dimension)
 pc2.shape = (NumberOfPoints2, Dimension)
 """
     
-def gromov_wasserstein(pc1, pc2): 
+def gromov_wasserstein(pc1:np.ndarray, pc2:np.ndarray)->float: 
     def dist_ecc_fast(ecc, u):
         return(np.mean(ecc <= u))
     
@@ -24,26 +26,21 @@ def gromov_wasserstein(pc1, pc2):
         
     return(0.5*out) 
     
-def chamfer_distance(pc1, pc2):
-    d = np.square(distance_matrix(pc1, pc2))
-
-    return d.min(1).mean() + d.min(0).mean()
+def chamfer_distance(pc1:np.ndarray, pc2:np.ndarray)->float:
+    dist = cdist(pc1, pc2)
+    ch_dist = (np.min(dist, axis=1).mean() + np.min(dist, axis=0).mean()) / 2
+    return ch_dist
     
-def average_ratio(pc1, pc2, Dist_list):
-    n = len(Dist_list)
-    d = np.square(distance_matrix(pc1, pc2))
+def average_ratio(pc1:np.ndarray, pc2:np.ndarray, Dist_list:list)->float:
+    d = cdist(pc1, pc2)
     avr = 0
-    for i in range(n):
-        dist = Dist_list[i]
+    for i,dist in enumerate(Dist_list):
         avr += (i+1) * ((d.min(1) <= dist).sum()/pc1.shape[0] + (d.min(0) <= dist).sum()/pc2.shape[0])
         
-    return avr / (n**2 + n)
+    return avr / (len(Dist_list)**2 + len(Dist_list))
     
-def ratio(pc1, pc2, thr_list = [0.1, 0.5, 1, 2, 4]):
-    d = np.square(distance_matrix(pc1, pc2))
-    r = []
-    for thr in thr_list:
-        r.append((d.min(1) <= thr).sum()/pc1.shape[0])
-
+def ratio(pc1:np.ndarray, pc2:np.ndarray, thr_list = np.array([0.1, 0.5, 1, 2, 4]))->list:
+    d = np.min(cdist(pc1, pc2), axis=1)
+    r = (d <= thr_list[:, None]).mean(1)
     return thr_list, r
 
